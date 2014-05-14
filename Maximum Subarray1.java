7274fd8214366aa4cd0a64d4edfc3f29e11d009c
kimiyaya1124 <xixuan1124@gmail.com>
2014-05-14 23:07:23 +0800

    Create Maximum Subarray1.java

---
+++ Maximum Subarray1.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxSubArray(int[] A) {
+        if(A.length==0)
+            return 0;
+        int maxs = Integer.MIN_VALUE;
+        int lastmaxs = 0;
+        int i;
+        for(i = 0;i < A.length;i++){
+            if(lastmaxs<0)
+                lastmaxs = 0;
+            lastmaxs += A[i];
+            if(lastmaxs>maxs)
+                maxs = lastmaxs;
+        }
+        return maxs;
+    }
+}
