d6832bdcf34e6faaa229edbe637500e2860408ca
kimiyaya1124 <xixuan1124@gmail.com>
2014-05-17 20:17:00 +0800

    Create Sort Colors.java

---
+++ Sort Colors.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public void sortColors(int[] A) {
+        if((A.length==0) || (A.length==1))
+            return;
+        
+        int h = 0, t = A.length-1;
+        while((h < A.length) && (A[h] == 0)){
+            h++;
+        }
+        while((t>=0) && (A[t] == 2)){
+            t--;
+        }
+        if((h >= A.length) || (t < 0) || (h>t))
+            return;
+            
+        int index = h;
+        while(index <= t){
+            if(A[index] == 2){
+                A[index] = A[t];
+                A[t] = 2;
+                t--;
+            }
+            else if(A[index] == 0){
+                if(index == h){
+                    index++;
+                }
+                else{
+                    A[index] = A[h];
+                    A[h] = 0;
+                }
+                h++;
+            }
+            else
+                index++;
+                
+            
+            if(h > t)
+                break;
+        }
+    }
+}
