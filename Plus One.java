12a9d7bc05930e5773d6855330f70ba8c0bc9aae
kimiyaya1124 <xixuan1124@gmail.com>
2014-05-19 23:22:45 +0800

    Update Plus One.java

--- Plus One.java
+++ Plus One.java
@@ -17,21 +17,11 @@ public class Solution {
         }
         else{
             int[] res = new int[digits.length];
-            int curdigit = 0,iscarry=0;
+            int curdigit = 0,iscarry=1;
             for(int i = digits.length-1;i >=0;i--){
-                if(i == digits.length-1){
-                    curdigit = digits[i]+1;
-                }
-                else{
-                    curdigit = digits[i] + iscarry;
-                    iscarry = 0;
-                }
-                if(curdigit>9){
-                    iscarry = 1;
-                    res[i] = 0;
-                }
-                else
-                    res[i] = curdigit;
+                curdigit = digits[i] + iscarry;
+                iscarry = curdigit / 10;
+                res[i] = curdigit % 10;
             }
             return res;
         }
