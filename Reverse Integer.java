bab9c861be75746bd584c8940cae86d3d19dc8f8
kimiyaya1124 <xixuan1124@gmail.com>
2014-11-14 01:01:04 +0800

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -1,11 +1,22 @@
 public class Solution {
+    private static final int maxDiv10 = Integer.MAX_VALUE / 10;
+    private static final int minDiv10 = Integer.MIN_VALUE / 10;
     public int reverse(int x) {
+        if(x == Integer.MAX_VALUE){
+            return 0;
+        }
+        if(x == Integer.MIN_VALUE){
+            return 0;
+        }
         int n = 0;
         int tx = x;
         while(tx != 0){
+            if(n > maxDiv10 || n < minDiv10){
+                return 0;
+            }
             n = n * 10 + tx % 10;
             tx = tx/10;
         }
-        return n;
+        return (int) n;
     }
 }
